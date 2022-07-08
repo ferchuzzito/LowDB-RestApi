53eae4cd4492f5b8921477507322239a46495144
FernandoLeon <ferzleon51@gmail.com>
2022-07-08 12:55:27 -0500

    create tasks

--- src/database/database.js
+++ src/database/database.js
@@ -5,10 +5,15 @@ import { fileURLToPath } from "url";
 let db;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export function createConnection() {
+export async function createConnection() {
   const file = join(__dirname, "../db.json");
   const adapter = new JSONFile(file);
   db = new Low(adapter);
+
+  await db.read();
+
+  db.data ||= { tasks: [] };
+  await db.write();
 }
 
 export const getConnection = () => db;
