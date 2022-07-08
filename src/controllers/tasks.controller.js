1ae075f5bd3b8f29c0c6431fa7854dc2e7b24f94
FernandoLeon <ferzleon51@gmail.com>
2022-07-08 13:49:27 -0500

    crear Tareas

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,25 @@
+import { v4 } from "uuid";
+import { getConnection } from "../database/database.js";
+
 export const getTasks = (req, res) => {
   res.send("sending tasks");
 };
 
-export const createTasks = (req, res) => {
-  res.send("sending tasks");
+export const createTasks = async (req, res) => {
+  const newTask = {
+    id: v4(),
+    name: req.body.name,
+    description: req.body.description,
+  };
+  try {
+    const db = getConnection();
+    const { tasks } = db.data;
+    tasks.push(newTask);
+    await db.write();
+    res.json(newTask);
+  } catch (error) {
+     return res.status(500).send(error);
+  }
 };
 
 export const getTask = (req, res) => {
@@ -18,5 +34,5 @@ export const deleteTasks = (req, res) => {
 };
 
 export const count = (req, res) => {
-     res.send("sending tasks");
-   };
+  res.send("sending tasks");
+};
