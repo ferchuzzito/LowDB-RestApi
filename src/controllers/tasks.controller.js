52a6dbd713d64d928b4beb5f48d01fded7705e94
FernandoLeon <ferzleon51@gmail.com>
2022-07-08 14:12:39 -0500

    obtener y eliminar tareas

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -2,10 +2,11 @@ import { v4 } from "uuid";
 import { getConnection } from "../database/database.js";
 
 export const getTasks = (req, res) => {
-  res.send("sending tasks");
+  const tasks = getConnection().data.tasks;
+  res.json(tasks);
 };
 
-export const createTasks = async (req, res) => {
+export const createTask = async (req, res) => {
   const newTask = {
     id: v4(),
     name: req.body.name,
@@ -18,19 +19,31 @@ export const createTasks = async (req, res) => {
     await db.write();
     res.json(newTask);
   } catch (error) {
-     return res.status(500).send(error);
+    return res.status(500).send(error);
   }
 };
 
 export const getTask = (req, res) => {
-  res.send("sending tasks");
+  const taskFound = getConnection().data.tasks.find(
+    (t) => t.id === req.params.id
+  );
+  if (!taskFound) res.sendStatus(404);
+  res.json(taskFound);
 };
 
-export const updateTasks = (req, res) => {
+export const updateTask = (req, res) => {
   res.send("sending tasks");
 };
-export const deleteTasks = (req, res) => {
-  res.send("sending tasks");
+export const deleteTask = async (req, res) => {
+  const db = getConnection();
+  const taskFound = db.data.tasks.find((t) => t.id === req.params.id);
+  if (!taskFound) res.sendStatus(404);
+
+  const newTasks = db.data.tasks.filter((t) => t.id !== req.params.id);
+  db.data.tasks = newTasks;
+  await db.write();
+
+  return res.json(taskFound);
 };
 
 export const count = (req, res) => {
