a8aad5c8c66db91e1415b3a2ddcf8cde49888f4d
FernandoLeon <ferzleon51@gmail.com>
2022-07-08 13:26:44 -0500

    configuracion de las rutas HTTP

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,22 @@
+export const getTasks = (req, res) => {
+  res.send("sending tasks");
+};
+
+export const createTasks = (req, res) => {
+  res.send("sending tasks");
+};
+
+export const getTask = (req, res) => {
+  res.send("sending tasks");
+};
+
+export const updateTasks = (req, res) => {
+  res.send("sending tasks");
+};
+export const deleteTasks = (req, res) => {
+  res.send("sending tasks");
+};
+
+export const count = (req, res) => {
+     res.send("sending tasks");
+   };
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import {
+  count,
+  createTasks,
+  deleteTasks,
+  getTask,
+  getTasks,
+  updateTasks,
+} from "../controllers/tasks.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/tasks', (req, res) => { res.send('sending tasks')} )
+router.get("/tasks", getTasks);
+router.get("/tasks/count", count);
+router.get("/tasks/:id", getTask);
+router.post("/tasks", createTasks);
+router.put("/tasks/:id", updateTasks);
+router.delete("/tasks/:id", deleteTasks);
 
 export default router;
